1c0229d82ecc23f94af10581dc4cc8b24c8548c1
howardks <93079698+howardks@users.noreply.github.com>
2023-09-18 21:43:11 -0400

    Add project files.

---
+++ CAT-App/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CAT_App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CAT-App/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CAT_App
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
