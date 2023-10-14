8c6ea61a4410a176779b1acb3609ccf53b4fb931
howardks <93079698+howardks@users.noreply.github.com>
2023-10-14 19:21:53 -0400

    NewsItemService unused imports removed

--- CAT-App/Data/NewsItemService.cs
+++ CAT-App/Data/NewsItemService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CAT_App.Data
+﻿namespace CAT_App.Data
 {
 	public class NewsItemService
 	{
