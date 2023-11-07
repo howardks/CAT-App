c61892c0ba56efa17d6a27a1bc66180f7421b87f
howardks <93079698+howardks@users.noreply.github.com>
2023-11-07 10:52:38 -0500

    Footer buttons fixed, User class updated

--- CAT-App/Data/User.cs
+++ CAT-App/Data/User.cs
@@ -2,9 +2,20 @@
 {
     public class User
     {
-        public static bool LoggedIn { get; set; } = false;
+        public static bool LoggedIn { get; set; } = true;
         public static string Username { get; set; }
         public static string Password { get; set; }
+        public static string Type { get; set; }
+        public static double Balance { get; set; }
+
+        public static void Logout()
+        {
+            LoggedIn = false;
+            Username = null;
+            Password = null;
+            Type = null;
+            Balance = 0;
+        }
 
         // Maybe stuff for connecting to api here? 
 
